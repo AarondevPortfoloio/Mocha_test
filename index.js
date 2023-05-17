a54b02c13ea7d4036644e89283e026e906f27355
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-05-17 16:47:36 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Calculate = {
+  factorial(num) {
+  if (num === 0 || num === 1){
+    return 1;
+  }
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+}
+
+module.exports = Calculate;
